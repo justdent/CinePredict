0fab69765dd61d2dafcb81454bae142d6c48116e
justdent <148442667+justdent@users.noreply.github.com>
2026-03-24 21:44:40 +0530

    Update preprocess.py

---
+++ data/preprocess.py
@@ -0,0 +1,11 @@
+mport pandas as pd 
+import numpy as np 
+import ast, re 
+  
+movies  = pd.read_csv('data/tmdb_5000_movies.csv') 
+credits = pd.read_csv('data/tmdb_5000_credits.csv.gz') 
+  
+print('Movies shape :', movies.shape) 
+print('Credits shape:', credits.shape) 
+print(movies.columns.tolist())
+rint(movies.isnull().sum()) 
